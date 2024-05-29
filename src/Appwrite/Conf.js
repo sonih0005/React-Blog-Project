1ce20842fde951bf47f13e119075627337cc1cf3
sonih0005 <sonih0005@gmail.com>
2024-05-29 11:59:12 +0530

    File upload service

--- src/Appwrite/Conf.js
+++ src/Appwrite/Conf.js
@@ -91,6 +91,39 @@ export class Service {
         console.log("Appwrite service :: Getpost :: error", error);
     }
   }
+
+  //file upload service-------- 
+  async uploadFile(file){
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      )
+    } catch (error) {
+      console.log("Appwrite service :: UploadFile :: error", error);
+    }
+  }
+
+  async deleteFile(fileId){
+    try {
+      await this.bucket.deleteFile(
+        conf.appwriteBucketId,
+        fileId
+      )
+      return true
+    } catch (error) {
+      console.log("Appwrite service :: DeleteFile :: error", error);
+      return false
+    }
+  }
+ 
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(
+      conf.appwriteBucketId,
+      fileId
+    )
+  }
 }
 
 const service = new Service();
